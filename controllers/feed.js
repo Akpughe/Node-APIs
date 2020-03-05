ae29e5f3ce82237bad1dab01f3651daecc20ef7a
David <davidakpughe2@gmail.com>
2020-03-05 10:34:19 +0100

    fetching list of post

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,15 +1,26 @@
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: 'First Post', content: 'This is the first post!' }]
+    posts: [
+      {
+        _id: '1',
+        title: 'First Post',
+        content: 'This is the first post!',
+        imageUrl: 'images/chimi.jpg',
+        creator: {
+          name: 'David'
+        },
+        createdAt: new Date()
+      }
+    ]
   });
 };
 
-exports.createPost = (req, res,next) => {
-    //Create a post in db
-    const title = req.body.title
-    const content = req.body.content
-    res.status(201).json({
-        message:"Post created succesfully!",
-        post:{id: new Date().toISOString(), title: title, content:content}
-    })
-}
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  //Create a post  in db
+  res.status(201).json({
+    message: 'Post created succesfully!',
+    post: { id: new Date().toISOString(), title: title, content: content }
+  });
+};
--- routes/feed.js
+++ routes/feed.js
@@ -7,7 +7,7 @@ const router = express.Router();
 //GET /feed/posts
 router.get('/posts', feedController.getPosts)
 
-// POST /feed/post
+// POST /feed/post 
 router.post('/post', feedController.createPost)
 
 module.exports = router;
