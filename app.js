8d08eb48a84b9c6ebc7a17945fdbd10fc7b9ff84
David <davidakpughe2@gmail.com>
2020-03-05 09:32:00 +0100

    REST API Clients CORS Errors - removing the CORS errors

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-const express = require('express')
+const express = require('express');
 
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
-const feedRoutes = require('./routes/feed')
+const feedRoutes = require('./routes/feed');
 
 const app = express();
 
-app.use(bodyParser.json()) //application/json
+app.use(bodyParser.json()); //application/json
 
-app.use('/feed', feedRoutes)
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT,PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
+app.use('/feed', feedRoutes);
 
-app.listen(8080)
+app.listen(8080);
