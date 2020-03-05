3b294eb6bc386c1af030e062a0a29bb84d1ad397
David <davidakpughe2@gmail.com>
2020-03-05 11:10:15 +0100

    setting up the post model

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const bodyParser = require('body-parser');
 
+const mongoose = require('mongoose');
+
 const feedRoutes = require('./routes/feed');
 
 const app = express();
@@ -17,4 +19,10 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+mongoose
+  .connect('mongodb+srv://nodejs:zxcvbnmlp@cluster0-dnqwk.mongodb.net/messages')
+  .then(result => {
+      app.listen(8080);
+  })
+  .catch(err => console.log(err));
+
