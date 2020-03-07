6f9f6c2bb23b6be543ee5bc9119372fd491a845b
David <davidakpughe2@gmail.com>
2020-03-07 19:43:49 +0100

    adding authorzation checks

--- controllers/feed.js
+++ controllers/feed.js
@@ -131,6 +131,11 @@ exports.updatePost = (req, res, next) => {
         error.statusCode = 404;
         throw error;
       }
+      if (post.creator.toString() !== req.userId) {
+        const error = new Error('Not Authorized');
+        error.statusCode = 403;
+        throw error;
+      }
       if (imageUrl !== post.image) {
         clearImage(post.imageUrl);
       }
@@ -156,6 +161,11 @@ exports.deletePost = (req, res, next) => {
       if (!post) {
         const error = new Error('Could not find post. ');
         error.statusCode = 404;
+        throw error;
+      }
+      if (post.creator.toString() !== req.userId) {
+        const error = new Error('Not Authorized');
+        error.statusCode = 403;
         throw error;
       }
       //Check logged in user
